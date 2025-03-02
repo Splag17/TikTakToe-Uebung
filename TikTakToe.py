fa5f0e02e89d7bef56690057b74a50018ed05c75
Splag17 <julian.eichhorn17@gmail.com>
2025-03-02 08:52:46 +0100

    Felderstellung mit Schleifen

--- TikTakToe.py
+++ TikTakToe.py
@@ -4,5 +4,21 @@ Spielfeld = [
             ["" ,"" ,""],
             ["" ,"" ,""]]
 
+#Spielfeld visualisieren
+def drawField():
+    Zeile = 3
+    Spalte = 2
 
+    for y in range(Zeile):
+        for x in range (Spalte):
+            print("         |         |         ")
 
+        if y < 2:
+            print("_________|_________|_________") 
+
+        #if y == 1:
+           # Spalte = 3 
+
+drawField()
+
+    
