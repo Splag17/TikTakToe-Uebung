86be1b73ef76dcb56b06f4c7ae2f58fce96b808c
Splag17 <julian.eichhorn17@gmail.com>
2025-03-03 15:19:00 +0100

    Add: Gewinnerprüfung

--- TikTakToe.py
+++ TikTakToe.py
@@ -34,7 +34,7 @@ def prüfe_eingabe(Zeile, Spalte, Spieler):
 
 
 # X oder O nach Eingabe ausgeben
-def print_xo(n,i):
+def place_xo(n,i):
 
     if Spielfeld[n-1][i-1] == 1:
         return("X")
@@ -48,15 +48,32 @@ def print_xo(n,i):
 def draw_field():
 
     print("        |        |        ")
-    print(f"   {print_xo(1,1)}    |    {print_xo(1,2)}   |    {print_xo(1,3)}    ")
+    print(f"   {place_xo(1,1)}    |    {place_xo(1,2)}   |    {place_xo(1,3)}    ")
     print("________|________|________") 
     print("        |        |        ")
-    print(f"   {print_xo(2,1)}    |    {print_xo(2,2)}   |    {print_xo(2,3)}    ")
+    print(f"   {place_xo(2,1)}    |    {place_xo(2,2)}   |    {place_xo(2,3)}    ")
     print("________|________|________") 
     print("        |        |        ")
-    print(f"   {print_xo(3,1)}    |    {print_xo(3,2)}   |    {print_xo(3,3)}    ")
+    print(f"   {place_xo(3,1)}    |    {place_xo(3,2)}   |    {place_xo(3,3)}    ")
     print("        |        |        ")
 
+
+#Kontrolle der Zeilen und Spalten der Matrix nach 3 in einer Reihe
+def check_win():
+    x = 1
+
+    while x <= 2:  
+        for z in range(3):
+ 
+            if Spielfeld[z] == [x, x, x]:
+                    print(f"Spieler{x} hat gewonnen!")  
+
+            elif all(Spielfeld[i][z] == x for i in range(3)):
+                    print(f"Spieler{x} hat gewonnen!") 
+
+            #Diagonal fehlt noch!    
+        x += 1     
+       
 
 #Allgemeiner Spielablauf
 draw_field()
@@ -65,3 +82,4 @@ for x in range(9):
     Zeile, Spalte, Spieler = spielzug(x)
     prüfe_eingabe(Zeile, Spalte, Spieler)
     draw_field()
+    check_win()
